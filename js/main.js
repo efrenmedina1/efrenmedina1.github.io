9abb4bced0842629effc3949270a4425f685b288
unknown <efrenmedina@live.com>
2019-04-28 23:01:49 -0400

    redesign

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 
-function go(loc) {
+function goIframe(loc) {
     
     document.getElementsByClassName('iframe')[0].scrollIntoView({
         behavior: 'smooth'
@@ -9,6 +9,28 @@ function go(loc) {
     document.getElementsByClassName('iframe')[0].contentWindow.location.reload();
     console.log('go')
     console.log(document.getElementsByClassName('iframe')[0].src)
-    
 
   }
+
+  function goHome() {
+    
+    document.getElementsByID('Home')[0].scrollIntoView({
+        behavior: 'smooth'
+      })
+    }
+    
+  function goProjects() {
+    console.log('projects')
+    
+    document.getElementById('projects').scrollIntoView({
+        behavior: 'smooth'
+      })
+    }
+
+    function goContact() {
+      console.log('Contact')
+      
+      document.getElementById('contact').scrollIntoView({
+          behavior: 'smooth'
+        })
+      }
